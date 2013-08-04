4d33389c5f05048245a453fef5fd47442a89940a
Maxim Krentovskiy <mkrentovskiy@gmail.com>
2013-08-04 22:17:37 +0200

    Relocate configuration files as source code want.

--- package/uaputl/uaputl.mk
+++ package/uaputl/uaputl.mk
@@ -7,8 +7,8 @@ endef
 
 define UAPUTL_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/uaputl $(TARGET_DIR)/usr/bin/uaputl
-    $(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/uaputl
-    $(INSTALL) -D -m 0644 $(@D)/config/* $(TARGET_DIR)/etc/uaputl/
+    $(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/share/uaputl
+    $(INSTALL) -D -m 0644 $(@D)/config/* $(TARGET_DIR)/usr/share/uaputl/
 endef
 
 $(eval $(generic-package))
