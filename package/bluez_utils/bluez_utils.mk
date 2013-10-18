2e0d7c12cd825fa0b58adf9748ec7c3a1f608d32
Gol <root@virt2real.(none)>
2013-10-18 11:40:44 -0400

    bluez-utils updated to 5.10

--- package/bluez_utils/bluez_utils.mk
+++ package/bluez_utils/bluez_utils.mk
@@ -4,12 +4,13 @@
 #
 #############################################################
 
-BLUEZ_UTILS_VERSION = 4.101
+#BLUEZ_UTILS_VERSION = 4.101
+BLUEZ_UTILS_VERSION = 5.10
 BLUEZ_UTILS_SOURCE = bluez-$(BLUEZ_UTILS_VERSION).tar.gz
 BLUEZ_UTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/bluetooth
 BLUEZ_UTILS_INSTALL_STAGING = YES
 BLUEZ_UTILS_DEPENDENCIES = dbus libglib2
-BLUEZ_UTILS_CONF_OPT = --enable-test --enable-tools
+BLUEZ_UTILS_CONF_OPT = --enable-test --enable-tools --disable-systemd
 BLUEZ_UTILS_AUTORECONF = YES
 BLUEZ_UTILS_LICENSE = GPLv2+ LGPLv2.1+
 BLUEZ_UTILS_LICENSE_FILES = COPYING COPYING.LIB
