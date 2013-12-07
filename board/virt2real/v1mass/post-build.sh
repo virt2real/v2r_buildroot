a63c04d77e04732a1b4ff05b5dd7d2738f7fd334
Gol <root@virt2real.(none)>
2013-12-07 12:12:27 -0500

    Added miniupnpc client

--- board/virt2real/v1mass/post-build.sh
+++ board/virt2real/v1mass/post-build.sh
@@ -11,7 +11,7 @@ sed -i 's%root::%root:8kfIfYHmcyQEE:%' $TARGETDIR/etc/shadow
 rm $TARGETDIR/etc/resolv.conf
 rm $TARGETDIR/etc/init.d/S01logging
 rm $TARGETDIR/etc/init.d/S10udev
-
+rm $TARGETDIR/etc/init.d/S99input-event-daemon
 
 # Copy the rootfs additions
 cp -r $BOARDDIR/rootfs-additions/* $TARGETDIR/
---
+++ package/miniupnpc/miniupnpc.mk
@@ -0,0 +1,30 @@
+#############################################################
+#
+# miniupnpc
+#
+#############################################################
+
+MINIUPNPC_VERSION = 1.8
+MINIUPNPC_SOURCE = miniupnpc-$(MINIUPNPC_VERSION).tar.gz
+MINIUPNPC_SITE = http://miniupnp.free.fr/files
+
+define MINIUPNPC_BUILD_CMDS
+        $(MAKE) -C $(@D) CC="$(TARGET_CC) $(TARGET_CFLAGS)"
+endef
+
+define MINIUPNPC_INSTALL_TARGET_CMDS
+        install -m 0755 -D $(@D)/upnpc-static $(TARGET_DIR)/usr/bin/upnpc
+endef
+
+define MINIUPNPC_INSTALL_TARGET_CMDS
+#	$(MAKE) -C $(@D)
+	cp -a $(@D)/upnpc-static $(TARGET_DIR)/usr/bin/upnpc
+endef
+
+define MINIUPNPC_CLEAN_CMDS
+#	$(MAKE) -C $(@D) clean
+	rm -f $(TARGET_DIR)/usr/bin/upnpc
+endef
+
+
+$(eval $(generic-package))
