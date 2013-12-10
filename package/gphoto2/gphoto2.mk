3cf1ff76c796ef75b9ce34251442f3e8ad7733f3
Maxim Krentovskiy <mkrentovskiy@gmsil.com>
2013-12-10 12:15:54 +0400

    Fixups for gphoto2 and gphotofs build.

--- package/gphoto2/gphoto2.mk
+++ package/gphoto2/gphoto2.mk
@@ -9,7 +9,7 @@ GPHOTO2_SOURCE = gphoto2-$(GPHOTO2_VERSION).tar.gz
 GPHOTO2_SITE = http://optimate.dl.sourceforge.net/project/gphoto/gphoto/2.5.2/
 GPHOTO2_INSTALL_STAGING = YES
 GPHOTO2_INSTALL_TARGET = YES
-GPHOTO2_DEPENDENCIES = host-libgphoto2 libusb-compat libgphoto2
-GPHOTO2_CONF_ENV += LDFLAGS+="-lgphoto2"
+GPHOTO2_DEPENDENCIES = popt libusb-compat libgphoto2 libexif
+GPHOTO2_CONF_OPT += --with-sysroot=$(STAGING_DIR) --with-libgphoto2=$(STAGING_DIR)/usr
 
 $(eval $(autotools-package))
--- package/gphotofs/gphotofs.mk
+++ package/gphotofs/gphotofs.mk
@@ -6,21 +6,19 @@
 
 GPHOTOFS_VERSION = 0.5
 GPHOTOFS_SOURCE = gphotofs-$(GPHOTOFS_VERSION).tar.gz
-#GPHOTOFS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/gphoto/gphotofs
 GPHOTOFS_SITE = http://citylan.dl.sourceforge.net/project/gphoto/gphotofs/0.5.0/
 GPHOTOFS_INSTALL_STAGING = YES
 GPHOTOFS_INSTALL_TARGET = YES
-GPHOTOFS_DEPENDENCIES = host-libgphoto2 libusb-compat libfuse libgphoto2 udev
-GPHOTOFS_CONF_ENV += LDFLAGS+="-lgphoto2"
+GPHOTOFS_DEPENDENCIES = libusb-compat libfuse libgphoto2 udev
+GPHOTOFS_CONF_OPT = --with-libgphoto2=$(STAGING_DIR)/usr
 
 define GPHOTOFS_INSTALL_UDEV
-        $(HOST_DIR)/usr/lib/libgphoto2/print-camera-list udev-rules version 175 mode 0660 > $(TARGET_DIR)/etc/udev/rules.d/40-gphoto.rules
+        # $(HOST_DIR)/usr/lib/libgphoto2/print-camera-list udev-rules version 175 mode 0660 > $(TARGET_DIR)/etc/udev/rules.d/40-gphoto.rules
         cat package/gphotofs/40-gphoto.rules >> $(TARGET_DIR)/etc/udev/rules.d/40-gphoto.rules
-        $(INSTALL) -m 0755 package/gphotofs//mountptp.sh $(TARGET_DIR)/usr/bin/mountptp.sh
-        $(INSTALL) -m 0755 package/gphotofs//umountptp.sh $(TARGET_DIR)/usr/bin/umountptp.sh
+        $(INSTALL) -m 0755 package/gphotofs/mountptp.sh $(TARGET_DIR)/usr/bin/mountptp.sh
+        $(INSTALL) -m 0755 package/gphotofs/umountptp.sh $(TARGET_DIR)/usr/bin/umountptp.sh
 endef
 
 GPHOTOFS_POST_INSTALL_TARGET_HOOKS += GPHOTOFS_INSTALL_UDEV
 
-#$(eval $(call AUTOTARGETS,package/thirdparty,gphotofs))
 $(eval $(autotools-package))
