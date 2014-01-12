0b90ef2b4cb2f4fdfebd0b797b42b83bec3a76fd
Maxim Krentovskiy <mkrentovskiy@gmsil.com>
2014-01-12 20:50:43 +0400

    Fixup deps of gst-rtsp-server

--- package/multimedia/gst-rtsp-server/gst-rtmp-server.mk
+++ package/multimedia/gst-rtsp-server/gst-rtmp-server.mk
@@ -7,6 +7,6 @@ GST_RTSP_SERVER_VERSION = 0.10.8
 GST_RTSP_SERVER_SITE = http://gstreamer.freedesktop.org/src/gst-rtsp-server/
 GST_RTSP_SERVER_SOURCE = gst-rtsp-$(GST_RTSP_SERVER_VERSION).tar.bz2
 
-GST_RTSP_SERVER_DEPENDENCIES = gstreamer
+GST_RTSP_SERVER_DEPENDENCIES = gstreamer gst-plugins-base
 
 $(eval $(autotools-package))
