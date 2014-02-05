73801277dec96eb0a8956c8d023f9beccf7de027
Gol <root@virt2real.(none)>
2014-02-05 18:46:11 -0500

    Some fixes

--- board/virt2real/v1mass/post-build.sh
+++ board/virt2real/v1mass/post-build.sh
@@ -8,6 +8,22 @@ BOARDDIR=board/virt2real/v1mass
 sed -i 's%root::%root:8kfIfYHmcyQEE:%' $TARGETDIR/etc/shadow
 
 # Remove bad files
+rm $TARGETDIR/etc/resolv.conf
+
+# Remove docs
+if [ -d $TARGETDIR/usr/share/gtk-doc ] ; then
+    rm -R $TARGETDIR/usr/share/gtk-doc
+fi
+
+if [ -d $TARGETDIR/usr/share/docs ] ; then
+    rm -R $TARGETDIR/usr/share/docs
+fi
+
+# Remove unused locale
+locales=`ls --ignore="ru" --ignore="en*" $TARGETDIR/usr/share/locale`
+for i in $locales ; do
+    rm -R $TARGETDIR/usr/share/locale/$i
+done
 
 # Copy the rootfs additions
-cp -r $BOARDDIR/rootfs-additions/* $TARGETDIR/
+cp -R $BOARDDIR/rootfs-additions/* $TARGETDIR/
