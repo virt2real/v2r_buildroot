cd5bdbb29f2902caa49321ba931bc84bee1ca9e3
Gol <root@virt2real.(none)>
2014-02-05 21:34:00 -0500

    Fixed locale remove

--- board/virt2real/v1mass/post-build.sh
+++ board/virt2real/v1mass/post-build.sh
@@ -8,6 +8,12 @@ BOARDDIR=board/virt2real/v1mass
 sed -i 's%root::%root:8kfIfYHmcyQEE:%' $TARGETDIR/etc/shadow
 
 # Remove bad files
+rm $TARGETDIR/etc/resolv.conf
+
+locales=`ls --ignore="ru" --ignore="en*" $TARGETDIR/usr/share/locale`
+for i in $locales ; do
+    rm -R $TARGETDIR/usr/share/locale/$i
+done
 
 # Copy the rootfs additions
 cp -r $BOARDDIR/rootfs-additions/* $TARGETDIR/
