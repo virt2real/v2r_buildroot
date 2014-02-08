2581eeae1c39904cff34efc36161515583f9da6c
Gol <hypergol@gmail.com>
2014-02-08 03:17:23 -0500

    Changed UBI and JFFS bootloader dumps

--- board/virt2real/v1mass/rootfs-additions/etc/virt2real/nand_flasher/nand_read.sh
+++ board/virt2real/v1mass/rootfs-additions/etc/virt2real/nand_flasher/nand_read.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+case "$1" in
+jffs)
+	DUMPNAME=jffs
+	;;
+ubi)
+	DUMPNAME=ubi
+	;;
+*)
+	echo "Usage: nand_read.sh ubi | jffs"
+	echo "    ubi - make ubi.gz dump file"
+	echo "    jffs - make jffs.gz dump file"	
+	exit 1
+	;;
+esac
+
 # get current LED state
 LED1=`cat /proc/v2r_gpio/73`
 LED2=`cat /proc/v2r_gpio/74`
@@ -15,7 +30,7 @@ echo "heartbeat" > "/sys/class/leds/v2r:green:user/trigger"
 
 # read UBL and U-boot image (bootloader NAND partition)
 nanddump -nof ./dump /dev/mtd0
-gzip -c ./dump > ./dump.gz
+gzip -c ./dump > ./$DUMPNAME.gz
 rm ./dump
 
 # turn off blinking green LED
@@ -25,4 +40,3 @@ echo "none" > "/sys/class/leds/v2r:green:user/trigger"
 echo $LED1 > /proc/v2r_gpio/73
 echo $LED2 > /proc/v2r_gpio/74
 echo $LED3 > /proc/v2r_gpio/pwctr3
-
--- board/virt2real/v1mass/rootfs-additions/etc/virt2real/nand_flasher/write_nand.sh
+++ board/virt2real/v1mass/rootfs-additions/etc/virt2real/nand_flasher/write_nand.sh
@@ -2,107 +2,189 @@
 
 #########################################################
 #
-#  NAND writer for Virt2real
+#  Kernel and filesystem to NAND writer for Virt2real
 #
 #  (c) 2014 Gol, special for Virt2real
 #
 #########################################################
 
-# get current LED state
-LED1=`cat /proc/v2r_gpio/73`
-LED2=`cat /proc/v2r_gpio/74`
-LED3=`cat /proc/v2r_gpio/pwctr3`
-
-# turn off all LEDs (but not led triggers)
-echo 0 > /proc/v2r_gpio/73
-echo 0 > /proc/v2r_gpio/74
-echo 0 > /proc/v2r_gpio/pwctr3
-
-# turn on blinking red LED
-echo "heartbeat" > "/sys/class/leds/v2r:red:user/trigger"
-
-
-# unzip bootloader image
-gzip -cd dump.gz> ./dump
-
-# write UBL and U-boot image to bootloader NAND partition
-nandwrite -no /dev/mtd0 ./dump
-
-# remove dump
-rm ./dump
+KERNEL_DST=/mnt/nand1
+ROOTFS_DST=/mnt/nand2
 
 # destination NAND partition for kernel
-NAND_ID=1
-KERNEL_DST=/mnt/nand$NAND_ID
+KERNEL_NAND_ID=1
 
-# clear kernel NAND partition
-flash_erase /dev/mtd$NAND_ID 0 0
+# destination NAND partition for fs
+ROOTFS_NAND_ID=2
 
-# mount MMC kernel partition
-mount /dev/mmcblk0p1 /mnt
 
-# write kernel to NAND
-nandwrite -p /dev/mtd$NAND_ID /mnt/uImage
+case "$1" in
+jffs)
+	DUMPNAME=jffs
+	;;
+ubi)
+	DUMPNAME=ubi
+	;;
+*)
+	echo "Usage: nand_write.sh ubi | jffs"
+	echo "    ubi - format as UBI"
+	echo "    jffs - format as JFFS"	
+	exit 1
+	;;
+esac
 
-# unmount kernel MMC partition
-umount /mnt
 
-# syncing all
-sync
+# turn on led blink
+led_on () {
 
-# destination NAND partition for rootfs
-NAND_ID=2
-ROOTFS_DST=/mnt/nand$NAND_ID
+	# get current LED state
+	LED1=`cat /proc/v2r_gpio/73`
+	LED2=`cat /proc/v2r_gpio/74`
+	LED3=`cat /proc/v2r_gpio/pwctr3`
 
-# clear rootfs NAND partition
-flash_erase /dev/mtd$NAND_ID 0 0
+	# turn off all LEDs (but not led triggers)
+	echo 0 > /proc/v2r_gpio/73
+	echo 0 > /proc/v2r_gpio/74
+	echo 0 > /proc/v2r_gpio/pwctr3
 
-# make directory for mounting
-mkdir -p $ROOTFS_DST
+	# turn on blinking red LED
+	echo "heartbeat" > "/sys/class/leds/v2r:red:user/trigger"
+	
+}
 
-# mount rootfs NAND partition
-mount -t jffs2 /dev/mtdblock$NAND_ID $ROOTFS_DST
 
-# make not regular directories
-mkdir -p $ROOTFS_DST/dev
-mkdir -p $ROOTFS_DST/mnt
-mkdir -p $ROOTFS_DST/proc
-mkdir -p $ROOTFS_DST/run
-mkdir -p $ROOTFS_DST/sys
-mkdir -p $ROOTFS_DST/tmp
+led_off () {
+	# turn off blinking red LED
+	echo "none" > "/sys/class/leds/v2r:red:user/trigger"
 
-for mntName in `ls /mnt`; do
-    mkdir -p $ROOTFS_DST/mnt/$mntName
-done
+	# restore LED state
+	echo $LED1 > /proc/v2r_gpio/73
+	echo $LED2 > /proc/v2r_gpio/74
+	echo $LED3 > /proc/v2r_gpio/pwctr3
+}
 
-# make symlink
-ln -s $ROOTFS_DST/bin/busybox $ROOTFS_DST/linuxrc
 
-# copy all regular directories and files to rootfs NAND partition
-for dirName in `ls /`; do
-    if [ "$dirName" == "dev" ] ; then echo "skip $dirName" ; continue ; fi
-    if [ "$dirName" == "mnt" ] ; then echo "skip $dirName" ; continue ; fi
-    if [ "$dirName" == "proc" ] ; then echo "skip $dirName" ; continue ; fi
-    if [ "$dirName" == "run" ] ; then echo "skip $dirName" ; continue ; fi
-    if [ "$dirName" == "sys" ] ; then echo "skip $dirName" ; continue ; fi
-    if [ "$dirName" == "tmp" ] ; then echo "skip $dirName" ; continue ; fi
+bootloader_write () {
+	# unzip bootloader image
+	gzip -cd $DUMPNAME.gz> ./dump
 
-    echo "copying $dirName"
-    cp -R /$dirName $ROOTFS_DST/
-done
+	# write UBL and U-boot image to bootloader NAND partition
+	nandwrite -no /dev/mtd0 ./dump
 
-# syncing (may be a long time)
-sync
+	# remove dump
+	rm ./dump
+}
 
-# unmount
-umount $ROOTFS_DST
-rmdir  $ROOTFS_DST
 
-# turn off blinking red LED
-echo "none" > "/sys/class/leds/v2r:red:user/trigger"
+kernel_write () {
+	# clear kernel NAND partition
+	flash_erase /dev/mtd$KERNEL_NAND_ID 0 0
 
-# restore LED state
-echo $LED1 > /proc/v2r_gpio/73
-echo $LED2 > /proc/v2r_gpio/74
-echo $LED3 > /proc/v2r_gpio/pwctr3
+	# mount MMC kernel partition
+	mkdir -p /mnt/mmc
+	mount /dev/mmcblk0p1 /mnt/mmc
 
+	# write kernel to NAND
+	nandwrite -p /dev/mtd$KERNEL_NAND_ID /mnt/mmc/uImage
+
+	# unmount and delete kernel MMC partition
+	umount /mnt/mmc
+	rm -Rf /mnt/mmc
+
+	# syncing all
+	sync
+}
+
+
+fs_format_jffs () {
+
+	# clear rootfs NAND partition
+	flash_erase /dev/mtd$ROOTFS_NAND_ID 0 0
+
+	# make directory for mounting
+	mkdir -p $ROOTFS_DST
+
+	# mount rootfs NAND partition
+	mount -t jffs2 /dev/mtdblock$ROOTFS_NAND_ID $ROOTFS_DST
+}
+
+
+fs_format_ubi () {
+
+	# clear rootfs NAND partition
+	flash_erase /dev/mtd$ROOTFS_NAND_ID 0 0
+
+	# make directory for mounting
+	mkdir -p $ROOTFS_DST
+
+	ubiformat /dev/mtd$ROOTFS_NAND_ID -s 512 -O 2048
+	ubiattach /dev/ubi_ctrl -m 2 -O 2048
+	ubimkvol /dev/ubi0 -N fs -s 230MiB
+
+	mount -t ubifs ubi0:fs $ROOTFS_DST
+}
+
+
+fs_write () {
+
+	# make not regular directories
+	mkdir -p $ROOTFS_DST/dev
+	mkdir -p $ROOTFS_DST/mnt
+	mkdir -p $ROOTFS_DST/proc
+	mkdir -p $ROOTFS_DST/run
+	mkdir -p $ROOTFS_DST/sys
+	mkdir -p $ROOTFS_DST/tmp
+
+	for mntName in `ls /mnt`; do
+    	mkdir -p $ROOTFS_DST/mnt/$mntName
+	done
+
+	# make symlink
+	ln -s $ROOTFS_DST/bin/busybox $ROOTFS_DST/linuxrc
+
+	# copy all regular directories and files to rootfs NAND partition
+	for dirName in `ls /`; do
+    	if [ "$dirName" == "dev" ] ; then echo "skip $dirName" ; continue ; fi
+	    if [ "$dirName" == "mnt" ] ; then echo "skip $dirName" ; continue ; fi
+    	if [ "$dirName" == "proc" ] ; then echo "skip $dirName" ; continue ; fi
+	    if [ "$dirName" == "run" ] ; then echo "skip $dirName" ; continue ; fi
+    	if [ "$dirName" == "sys" ] ; then echo "skip $dirName" ; continue ; fi
+	    if [ "$dirName" == "tmp" ] ; then echo "skip $dirName" ; continue ; fi
+
+    	echo "copying $dirName"
+	    cp -R /$dirName $ROOTFS_DST/
+	done
+
+	# syncing (may be a long time)
+	sync
+}
+
+fs_unmount () {
+	# unmount fs dir
+	echo "unmounting $ROOTFS_DST"
+	umount $ROOTFS_DST
+	echo "removing $ROOTFS_DST"
+	rmdir  $ROOTFS_DST
+}
+
+
+
+led_on
+bootloader_write
+kernel_write
+
+case "$1" in
+jffs)
+	fs_format_jffs
+	;;
+ubi)
+	fs_format_ubi
+	;;
+*)
+	exit 1
+	;;
+esac
+
+fs_write
+fs_unmount
+led_off
