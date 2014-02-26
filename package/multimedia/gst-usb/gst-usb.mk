89b589eebb7fbc942e3377ecbd0c362374bc5f3a
Maxim Krentovskiy <mkrentovskiy@gmail.com>
2014-02-26 23:07:31 +0400

    Fix path in gstd. Start working on gst-plugin-usb and ipiped.

---
+++ package/multimedia/gst-usb/gst-usb.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# gst-usb
+#
+#############################################################
+GST_USB_VERSION = HEAD
+GST_USB_SITE = http://github.com/RidgeRun/gst-plugin-usb/tarball/$(GST_USB_VERSION)
+
+GST_USB_DEPENDENCIES = gstreamer gst-plugins-base
+
+$(eval $(autotools-package))
--- package/multimedia/gstd/gstd.mk
+++ package/multimedia/gstd/gstd.mk
@@ -9,7 +9,7 @@ GSTD_SITE = http://github.com/RidgeRun/gstd/tarball/$(GSTD_VERSION)
 #GSTD_INSTALL_STAGING = YES
 GSTD_INSTALL_TARGET = YES
 GSTD_AUTORECONF = YES
-GSTD_DEPENDENCIES = host-pkgconf
+GSTD_DEPENDENCIES = host-pkgconf host-vala dbus
 GSTD_CONF_OPT = --with-vapidir=$(TARGET_DIR)/usr/share/vala-0.18/vapi/
 
 $(eval $(autotools-package))
