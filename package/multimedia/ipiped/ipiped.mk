c94e7b57d3c2420fa148696dab90310f6d09e8a6
Gol <hypergol@gmail.com>
2014-02-27 00:46:24 +0400

    Fixed ipiped but not building yet

--- package/multimedia/ipiped/ipiped.mk
+++ package/multimedia/ipiped/ipiped.mk
@@ -6,10 +6,8 @@
 
 IPIPED_VERSION = HEAD
 IPIPED_SITE = http://github.com/RidgeRun/ipiped/tarball/$(IPIPED_VERSION)
-#IPIPED_INSTALL_STAGING = YES
 IPIPED_INSTALL_TARGET = YES
-IPIPED_AUTORECONF = YES
-IPIPED_DEPENDENCIES = host-pkgconf host-vala
-IPIPED_CONF_OPT = --with-vapidir=$(TARGET_DIR)/usr/share/vala-0.18/vapi/
+GSTD_DEPENDENCIES = host-pkgconf host-vala dbus
+GSTD_CONF_OPT = --with-vapidir=$(TARGET_DIR)/usr/share/vala-0.18/vapi/
 
 $(eval $(autotools-package))
