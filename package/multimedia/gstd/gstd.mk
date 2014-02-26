132a2568bcc2b7a28bdfb1ac327753f7b91816f4
Gol <hypergol@gmail.com>
2014-02-27 00:39:29 +0400

    Fixed install binaries gstd

--- package/multimedia/gstd/gstd.mk
+++ package/multimedia/gstd/gstd.mk
@@ -14,10 +14,16 @@ GSTD_CONF_OPT = --with-vapidir=$(TARGET_DIR)/usr/share/vala-0.18/vapi/
 define GSTD_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/etc/dbus-1/system.d
 	$(INSTALL) -m 0755 package/multimedia/gstd/gstd.conf $(TARGET_DIR)/etc/dbus-1/system.d
+
+	mkdir -p $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/src/gstd $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/src/gst-client $(TARGET_DIR)/usr/bin
 endef
 
-define PROFTPD_UNINSTALL_TARGET_CMDS
+define GSTD_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/etc/dbus-1/system.d/gstd.conf
+	rm -f $(TARGET_DIR)/usr/bin/gstd
+	rm -f $(TARGET_DIR)/usr/bin/gst-client
 endef
 
 $(eval $(autotools-package))
