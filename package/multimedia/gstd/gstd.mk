f4147707da053be7eeb4ee05654025fce0d19956
Gol <hypergol@gmail.com>
2014-02-26 18:02:39 +0400

    Added gstd, but not builded yet

---
+++ package/multimedia/gstd/gstd.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# gstd
+#
+#############################################################
+
+GSTD_VERSION = HEAD
+GSTD_SITE = http://github.com/RidgeRun/gstd/tarball/$(GSTD_VERSION)
+#GSTD_INSTALL_STAGING = YES
+GSTD_INSTALL_TARGET = YES
+GSTD_AUTORECONF = YES
+GSTD_DEPENDENCIES = host-pkgconf
+GSTD_CONF_OPT = --with-vapidir=$(TARGET_DIR)/usr/share/vala-0.18/vapi/
+
+$(eval $(autotools-package))
