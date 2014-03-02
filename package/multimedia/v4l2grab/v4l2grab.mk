27b83fde8948614fc8cc094e1d8e78b9b99706e0
Gol <hypergol@gmail.com>
2014-03-02 23:43:33 +0400

    Added v4l2grab

---
+++ package/multimedia/v4l2grab/v4l2grab.mk
@@ -0,0 +1,22 @@
+#############################################################
+#
+# v4l2grab
+#
+#############################################################
+
+V4L2GRAB_VERSION = HEAD
+V4L2GRAB_SITE = http://github.com/twam/v4l2grab/tarball/$(V4L2GRAB_VERSION)
+V4L2GRAB_INSTALL_TARGET = YES
+V4L2GRAB_AUTORECONF = YES
+
+define V4L2GRAB_INSTALL_TARGET_CMDS
+
+	mkdir -p $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/v4l2grab $(TARGET_DIR)/usr/bin
+endef
+
+define V4L2GRAB_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/v4l2grab
+endef
+
+$(eval $(autotools-package))
