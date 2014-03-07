b660ecf08f9a41041dcbac944467d44debad5cf0
Gol <hypergol@gmail.com>
2014-03-08 00:20:57 +0400

    Fixed gstd dependences

--- package/multimedia/gstd/gstd.mk
+++ package/multimedia/gstd/gstd.mk
@@ -8,7 +8,7 @@ GSTD_VERSION = HEAD
 GSTD_SITE = http://github.com/RidgeRun/gstd/tarball/$(GSTD_VERSION)
 GSTD_INSTALL_TARGET = YES
 GSTD_AUTORECONF = YES
-GSTD_DEPENDENCIES = host-pkgconf host-vala dbus
+GSTD_DEPENDENCIES = host-pkgconf host-vala dbus gstreamer gst-plugins-base
 GSTD_CONF_OPT = --with-vapidir=$(TARGET_DIR)/usr/share/vala-0.18/vapi/
 
 define GSTD_INSTALL_TARGET_CMDS
