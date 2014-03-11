df0b82b82712a8ce8ab4abd5e9c36a17b61013dd
Gol <hypergol@gmail.com>
2014-03-11 06:40:08 +0400

    Changed Spginx package

---
+++ package/multimedia/sphinxbase/sphinxbase.mk
@@ -0,0 +1,9 @@
+SPHINXBASE_VERSION=0.6.1
+SPHINXBASE_SOURCE=sphinxbase-$(SPHINXBASE_VERSION).tar.gz
+SPHINXBASE_SITE=http://downloads.sourceforge.net/project/cmusphinx/sphinxbase/$(SPHINXBASE_VERSION)
+SPHINXBASE_AUTORECONF = YES
+SPHINXBASE_INSTALL_STAGING = YES
+SPHINXBASE_INSTALL_TARGET = YES
+SPHINXBASE_CONF_OPT = --without-python
+
+$(eval $(autotools-package))
