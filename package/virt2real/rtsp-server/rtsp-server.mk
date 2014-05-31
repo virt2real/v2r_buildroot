800af0003939e95272e196d8c42c91d97ce8561d
Gol <hypergol@gmail.com>
2014-05-31 16:40:53 +0400

    Changed autostart script for rtsp server

--- package/virt2real/rtsp-server/rtsp-server.mk
+++ package/virt2real/rtsp-server/rtsp-server.mk
@@ -14,13 +14,16 @@ endef
 define RTSP_SERVER_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin
 	mkdir -p $(TARGET_DIR)/etc/virt2real
+	mkdir -p $(TARGET_DIR)/etc/init.d.sample
 	$(INSTALL) -m 0755 -D $(@D)/rtsp-server/rtsp-server $(TARGET_DIR)/usr/bin/rtsp-server
 	$(INSTALL) -m 0755 package/virt2real/rtsp-server/rtsp_server.sh $(TARGET_DIR)/etc/virt2real/rtsp_server.sh
+	$(INSTALL) -m 0755 package/virt2real/rtsp-server/S92rtspserver $(TARGET_DIR)/etc/init.d.sample/S92rtspserver
 endef
 
 define RTSP_SERVER_CLEAN_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/rtsp-server
 	rm -f $(TARGET_DIR)/etc/virt2real/rtsp_server.sh
+	rm -f $(TARGET_DIR)/etc/init.d.sample/S92rtspserver
 endef
 
 $(eval $(generic-package))
