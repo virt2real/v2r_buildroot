c7221b0dce8fe3b785bd7b839ffaa99add77c6a7
Gol <hypergol@gmail.com>
2014-05-31 14:50:53 +0400

    Fixed rtsp_server.sh

--- package/virt2real/rtsp-server/rtsp_server.sh
+++ package/virt2real/rtsp-server/rtsp_server.sh
@@ -40,22 +40,22 @@ start)
 		echo "RTSP" > /tmp/onair
 		if [ "$VERSION" == "1" ] ;
 		then
-			killall test-launch > /dev/null 2>&1
-			rtsp-server $PORT $PATH "( v4l2src queue-size=2 always-copy=false chain-ipipe=true ! video/x-raw-yuv,width=1280,height=720 ! queue ! dmaiaccel ! dmaienc_h264 ddrbuf=false copyOutput=false outputBufferSize=1500000 targetbitrate=$BITRATE encodingpreset=2 ratecontrol=1 intraframeinterval=23 idrinterval=46 level=41 qpintra=25 qpinter=25 bytestream=true ! rtph264pay pt=96 name=pay0 mtu=3000 config-interval=0 perfect-rtptime=false scan-mode=1 )"
+			/usr/bin/killall test-launch > /dev/null 2>&1
+			/usr/bin/rtsp-server $PORT $PATH "( v4l2src queue-size=2 always-copy=false chain-ipipe=true ! video/x-raw-yuv,width=1280,height=720 ! queue ! dmaiaccel ! dmaienc_h264 ddrbuf=false copyOutput=false outputBufferSize=1500000 targetbitrate=$BITRATE encodingpreset=2 ratecontrol=1 intraframeinterval=23 idrinterval=46 level=41 qpintra=25 qpinter=25 bytestream=true ! rtph264pay pt=96 name=pay0 mtu=3000 config-interval=0 perfect-rtptime=false scan-mode=1 )"
 		else
-			killall gst-launch-0.10 > /dev/null 2>&1
-			gst-launch v4l2src queue-size=2 always-copy=false chain-ipipe=true ! video/x-raw-yuv,format='(fourcc)'NV12, width=1280, height=720, framerate='(fraction)'30/1 ! queue ! dmaiaccel ! dmaienc_h264 ddrbuf=false copyOutput=false outputBufferSize=9000000 targetbitrate=$BITRATE encodingpreset=2 ratecontrol=2 intraframeinterval=10 idrinterval=20 level=41 ! queue ! rtspsink mapping="$PATH" service="$PORT"
+			/usr/bin/killall gst-launch-0.10 > /dev/null 2>&1
+			/usr/bin/gst-launch v4l2src queue-size=2 always-copy=false chain-ipipe=true ! video/x-raw-yuv,format='(fourcc)'NV12, width=1280, height=720, framerate='(fraction)'30/1 ! queue ! dmaiaccel ! dmaienc_h264 ddrbuf=false copyOutput=false outputBufferSize=9000000 targetbitrate=$BITRATE encodingpreset=2 ratecontrol=2 intraframeinterval=10 idrinterval=20 level=41 ! queue ! rtspsink mapping="$PATH" service="$PORT"
 		fi
 		echo "" > /tmp/onair
 
-		sleep 10
+		/bin/sleep 10
 	done
 
 	;;
 
 stop)
-	killall rtsp-server > /dev/null 2>&1
-	killall gst-launch-0.10 > /dev/null 2>&1
+	/usr/bin/killall rtsp-server > /dev/null 2>&1
+	/usr/bin/killall gst-launch-0.10 > /dev/null 2>&1
 	echo "" > /tmp/onair
 	;;
 esac
