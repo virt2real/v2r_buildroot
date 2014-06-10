fbf713e63c3e23939c4d8e7a3329e24db18397b2
Gol <hypergol@gmail.com>
2014-06-10 16:39:21 +0400

    Added camera mode set in autostart

--- board/virt2real/v1mass/rootfs-additions/etc/virt2real/autostart.sh
+++ board/virt2real/v1mass/rootfs-additions/etc/virt2real/autostart.sh
@@ -10,10 +10,7 @@ export NODE_PATH=/usr/lib/node_modules
 /usr/bin/amixer set "Mono DAC" 63 > /dev/null
 
 # add broadcast route
-/sbin/ip route add 224.0.0.0/4 dev wlan0
-
-# camera flip
-#i2cset -f -y 1 0x30 0x12 0x39
+#/sbin/ip route add 224.0.0.0/4 dev wlan0
 
 # TCP/IP tune
 sysctl -w net.ipv4.tcp_congestion_control=lp > /dev/null
