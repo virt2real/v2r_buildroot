81b60590c7b839f3a7db8dec5e3cee3447d0d937
Gol <hypergol@gmail.com>
2014-06-17 15:33:11 +0400

    Added i2c_write

---
+++ package/virt2real/i2c_write/i2c_write.mk
@@ -0,0 +1,23 @@
+#############################################################
+#
+# i2c_write
+#
+#############################################################
+
+I2C_WRITE_VERSION = HEAD
+I2C_WRITE_SITE = http://github.com/virt2real/othersoft/tarball/$(I2C_WRITE_VERSION)
+
+define I2C_WRITE_BUILD_CMDS
+        $(MAKE) -C $(@D)/i2c_write CC="$(TARGET_CC) $(TARGET_CFLAGS)"
+endef
+
+define I2C_WRITE_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 -D $(@D)/i2c_write/i2c_write $(TARGET_DIR)/usr/bin/i2c_write
+endef
+
+define I2C_WRITE_CLEAN_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/i2c_write
+endef
+
+$(eval $(generic-package))
---
+++ package/virt2real/rcboard/rcboard.mk
@@ -0,0 +1,29 @@
+#############################################################
+#
+# rcboard
+#
+#############################################################
+
+RCBOARD_VERSION = HEAD
+RCBOARD_SITE = http://github.com/virt2real/rcboard/tarball/$(RCBOARD_VERSION)
+
+define RCBOARD_BUILD_CMDS
+        $(MAKE) -C $(@D)/board CC="$(TARGET_CC) $(TARGET_CFLAGS)"
+endef
+
+define RCBOARD_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/opt/rcboard
+	mkdir -p $(TARGET_DIR)/etc/init.d.sample
+	$(INSTALL) -m 0755 -D $(@D)/board/rcboard $(TARGET_DIR)/opt/rcboard/
+	$(INSTALL) -m 0755 -D $(@D)/board/scripts/*.sh $(TARGET_DIR)/opt/rcboard/
+	$(INSTALL) -m 0644 -D $(@D)/board/scripts/rcboard.conf $(TARGET_DIR)/opt/rcboard/
+	$(INSTALL) -m 0755 package/virt2real/rcboard/S98rcboard $(TARGET_DIR)/etc/init.d.sample/
+endef
+
+define RCBOARD_CLEAN_CMDS
+	rm -f $(TARGET_DIR)/opt/rcboard/rcboard
+	rm -f $(TARGET_DIR)/opt/rcboard/*
+	rm -f $(TARGET_DIR)/etc/init.d.sample/S98rcboard
+endef
+
+$(eval $(generic-package))
