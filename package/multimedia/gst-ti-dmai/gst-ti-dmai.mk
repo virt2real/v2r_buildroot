d2f16abeb007d3d220e832a88cc5b1a39b0a069a
Maxim Krentovskiy <mkrentovskiy@gmail.com>
2014-08-12 00:13:42 +0400

    Rename ti-gstreamer-dmai to gst-ti-dmai. Start working on build it with SDK. Update multimedia packages order.

--- package/multimedia/gst-ti-dmai/gst-ti-dmai.mk
+++ package/multimedia/gst-ti-dmai/gst-ti-dmai.mk
@@ -1,24 +1,26 @@
 ################################################################################
 #
-# ti-dmai
+# gst-ti-dmai
 #
 ################################################################################
 
-TI_GSTREAMER_DMAI_VERSION = HEAD
-TI_GSTREAMER_DMAI_SITE = http://github.com/virt2real/gstreamer-dmai/tarball/$(TI_GSTREAMER_DMAI_VERSION)
-TI_GSTREAMER_DMAI_INSTALL_STAGING = YES
-TI_GSTREAMER_DMAI_DEPENDENCIES = gstreamer host-pkgconf
+GST_TI_DMAI_VERSION = HEAD
+GST_TI_DMAI_SITE = http://github.com/virt2real/gstreamer-dmai/tarball/$(GST_TI_DMAI_VERSION)
+GST_TI_DMAI_INSTALL_STAGING = YES
+GST_TI_DMAI_DEPENDENCIES = gstreamer host-pkgconf
+GST_TI_DMAI_AUTORECONF = YES
 
-TI_GSTREAMER_DMAI_CONF_OPT = \
+GST_TI_DMAI_CONF_OPT = \
 	--with-preset=dm365  \
 	--with-platform=dm365 \
 	--target=arm-none-linux-gnueabi \
-	--host=arm-none-linux-gnueabi
+	--host=arm-none-linux-gnueabi \
+	--with-sysroot=$(STAGING_DIR)
 
-TI_GSTREAMER_DMAI_PLATFORM = dm365
+GST_TI_DMAI_PLATFORM = dm365
 
-TI_GSTREAMER_DMAI_CONF_ENV = \
-		PLATFORM="$(TI_GSTREAMER_DMAI_PLATFORM)" \
+GST_TI_DMAI_CONF_ENV = \
+		PLATFORM="$(GST_TI_DMAI_PLATFORM)" \
 		XDAIS_INSTALL_DIR="$(DEVDIR)/dvsdk/xdais_6_26_01_03" \
 		CE_INSTALL_DIR="$(DEVDIR)/dvsdk/codec-engine_2_26_02_11" \
 		CODEC_INSTALL_DIR="$(DEVDIR)/dvsdk/codecs-dm365_4_02_00_00" \
@@ -30,9 +32,10 @@ TI_GSTREAMER_DMAI_CONF_ENV = \
 		XDC_USER_PATH="$(DEVDIR)/dvsdk/xdctools_3_16_03_36/examples" \
 		CROSS_COMPILE=$(TARGET_CROSS) \
 		CSTOOL_DIR="$(HOST_DIR)/usr" \
+		MVTOOL_DIR="$(HOST_DIR)/usr" \
 		CMEM_INSTALL_DIR="$(DEVDIR)/dvsdk/linuxutils_2_26_03_06" \
 		DMAI_INSTALL_DIR="$(DEVDIR)/dvsdk/dmai_2_20_00_15"
 
-TI_GSTREAMER_DMAI_MAKE_ENV = $(TI_GSTREAMER_DMAI_CONF_ENV)
+GST_TI_DMAI_MAKE_ENV = $(GST_TI_DMAI_CONF_ENV)
 
 $(eval $(autotools-package))
