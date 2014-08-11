06f69d399a43c7022b0d3ff008689e3c2a7ba43b
Maxim Krentovskiy <mkrentovskiy@gmail.com>
2014-08-12 00:12:21 +0400

    Make gst-usb compiling.

--- package/multimedia/gst-usb/gst-usb.mk
+++ package/multimedia/gst-usb/gst-usb.mk
@@ -5,6 +5,7 @@
 #############################################################
 GST_USB_VERSION = HEAD
 GST_USB_SITE = http://github.com/RidgeRun/gst-plugin-usb/tarball/$(GST_USB_VERSION)
+GST_USB_AUTORECONF = YES
 
 GST_USB_DEPENDENCIES = gstreamer gst-plugins-base
 
