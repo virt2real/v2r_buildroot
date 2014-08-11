4f7479f6117c6f9bbd61edc29562d4015549ce9f
Gol <hypergol@gmail.com>
2014-08-11 04:02:57 +0400

    PSplash added

---
+++ package/multimedia/psplash/psplash.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# psplash
+#
+################################################################################
+
+PSPLASH_VERSION = afd4e228c606a9998feae44a3fed4474803240b7
+PSPLASH_SITE = git://git.yoctoproject.org/psplash
+PSPLASH_LICENSE = GPLv2+
+PSPLASH_AUTORECONF = YES
+
+
+define PSPLASH_BUILD_CMDS
+        cp package/multimedia/psplash/screenlogo.png $(@D)/base-images/screenlogo.png
+	cd $(@D) && $(@D)/make-image-header.sh $(@D)/base-images/screenlogo.png POKY && mv screenlogo-img.h psplash-poky-img.h
+endef
+
+
+$(eval $(autotools-package))
