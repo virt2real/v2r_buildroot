d16e459ae3aa79578972b00fe8eeb213c09321b8
Gol <hypergol@gmail.com>
2014-08-26 04:05:43 +0400

    Changed libnfc version, added support for np532

--- package/libnfc/libnfc.mk
+++ package/libnfc/libnfc.mk
@@ -1,9 +1,10 @@
-#############################################################
+################################################################################
 #
 # libnfc
 #
-#############################################################
-LIBNFC_VERSION = fda8d60ce062
+################################################################################
+
+LIBNFC_VERSION = 7b7f5061427b5456835dd48923a8cc0563cfd1e9
 LIBNFC_SITE = http://libnfc.googlecode.com/git/
 LIBNFC_SITE_METHOD = git
 LIBNFC_LICENSE = LGPLv3+
@@ -15,6 +16,9 @@ LIBNFC_DEPENDENCIES = host-pkgconf libusb libusb-compat
 
 # N.B. The acr122 driver requires pcsc-lite.
 LIBNFC_CONF_OPT = --with-drivers=arygon,pn53x_usb
+#LIBNFC_CONF_OPT = --with-drivers=arygon,pn532_uart --enable-serial-autoprobe
+#LIBNFC_CONF_OPT = --with-drivers=arygon,pn532_uart --enable-debug
+
 
 ifeq ($(BR2_PACKAGE_LIBNFC_EXAMPLES),y)
 LIBNFC_CONF_OPT += --enable-example
