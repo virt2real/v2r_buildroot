8571afa8ab2660b571e3bffc2a8cd3ca1be9a29f
root <root@v2rsdk.(none)>
2014-09-18 22:42:11 +0400

    Fix wget error about gettext infrastructure mismatch.

--- package/wget/wget.mk
+++ package/wget/wget.mk
@@ -1,13 +1,25 @@
-#############################################################
+################################################################################
 #
 # wget
 #
-#############################################################
+################################################################################
 
-WGET_VERSION = 1.14
+WGET_VERSION = 1.15
+WGET_SOURCE = wget-$(WGET_VERSION).tar.xz
 WGET_SITE = $(BR2_GNU_MIRROR)/wget
 WGET_LICENSE = GPLv3+
 WGET_LICENSE_FILES = COPYING
+
+# patching gnulib .m4 file
+WGET_AUTORECONF = YES
+
+# Ugly kludge to fix autoreconf with old gettext infra
+# We need to gettextize before autoreconf to upgrade
+WGET_DEPENDENCIES += host-gettext
+define WGET_GETTEXTIZE
+	cd $(@D) ; $(HOST_DIR)/usr/bin/gettextize -f
+endef
+WGET_POST_PATCH_HOOKS += WGET_GETTEXTIZE
 
 # Prefer full-blown wget over busybox
 ifeq ($(BR2_PACKAGE_BUSYBOX),y)
@@ -23,6 +35,10 @@ endif
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 	WGET_CONF_OPT += --with-ssl=openssl --with-libssl-prefix=$(STAGING_DIR)
 	WGET_DEPENDENCIES += openssl
+endif
+
+ifeq ($(BR2_PACKAGE_UTIL_LINUX_LIBUUID),y)
+	WGET_DEPENDENCIES += util-linux
 endif
 
 # --with-ssl is default
