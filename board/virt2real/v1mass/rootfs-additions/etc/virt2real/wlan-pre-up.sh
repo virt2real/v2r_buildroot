999ecfd51bfe72593956e340d66cf66a5211de01
Gol <hypergol@gmail.com>
2014-10-09 15:50:50 +0400

    Added Wi-Fi SSID and password settings from uEnv.txt

--- board/virt2real/v1mass/rootfs-additions/etc/virt2real/wlan-pre-up.sh
+++ board/virt2real/v1mass/rootfs-additions/etc/virt2real/wlan-pre-up.sh
@@ -9,9 +9,27 @@ if [ ! "$AP" == "" ] ; then
 	exit 0
 fi
 
+SED=/usr/bin/sed
+
 if [ -e  /var/run/wpa_supplicant/$IFACE ] ; then
-	rm /var/run/wpa_supplicant/$IFACE
+    rm /var/run/wpa_supplicant/$IFACE
 fi
 
+# get ssid from cmdline
+SSID=`cat /proc/cmdline |  $SED 's/ /\n/g' | grep wifi_ssid | $SED 's/wifi_ssid=//g'`
+
+# get psk from cmdline
+PASS=`cat /proc/cmdline |  $SED 's/ /\n/g' | grep wifi_pass | $SED 's/wifi_pass=//g'`
+
+if [ "$SSID" != "" ] ; then
+    if [ "$PASS" != "" ] ; then
+	# save current wpa_supplicant.conf
+	cp /etc/wpa_supplicant.conf /etc/wpa_supplicant.old
+	# change ssid and psk in wpa_supplicant.conf        
+	cat /etc/wpa_supplicant.old | $SED '0,/ssid=".*"/{s/ssid=".*"/ssid="'$SSID'"/}' | $SED '0,/psk=".*"/{s/psk=".*"/psk="'$PASS'"/}' > /etc/wpa_supplicant.conf
+    fi
+fi
+
+# run wpa_supplicant and wpa_cli
 nice -n -20 /usr/sbin/wpa_supplicant -D wext -i$IFACE -c /etc/wpa_supplicant.conf -B
 /usr/sbin/wpa_cli -i$IFACE -a /etc/virt2real/wlanchanged.sh -B
