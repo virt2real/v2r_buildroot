624b4c17382e47984381423ff59c2318c65a059f
Gol <hypergol@gmail.com>
2014-12-10 07:08:32 +0400

    Added sound for RTMP client

--- board/virt2real/v1mass/rootfs-additions/etc/virt2real/rtmp_client.sh
+++ board/virt2real/v1mass/rootfs-additions/etc/virt2real/rtmp_client.sh
@@ -11,15 +11,43 @@ fi
 
 . /etc/virt2real/rtmp_client.conf
 
-case "$1" in
-start)
-	/usr/bin/killall -INT gst-launch-0.10 > /dev/null 2>&1
+rtmp_no_sound()
+{
 	gst-launch v4l2src always-copy=false chain-ipipe=true ! \
 		capsfilter caps=video/x-raw-yuv,format='(fourcc)'NV12,width=$WIDTH,height=$HEIGHT,framerate='(fraction)'$FPS$PITCH ! \
 		dmaiaccel ! \
-		dmaienc_h264 copyOutput=false ddrbuf=false encodingpreset=2 ratecontrol=2 intraframeinterval=30 idrinterval=120 t8x8intra=true t8x8inter=true targetbitrate=$BITRATE bytestream=false headers=false ! \
+		dmaienc_h264 copyOutput=false ddrbuf=false encodingpreset=2 ratecontrol=2 single-nalu=true targetbitrate=$BITRATE maxbitrate=$BITRATE level=51 intraframeinterval=150 idrinterval=250 t8x8intra=true t8x8inter=true bytestream=false headers=false ! \
 		flvmux name=mux streamable=true ! \
-		rtmpsink location="$LOCATION" sync=false enable-last-buffer=false > /dev/null &
+		queue ! \
+		rtmpsink location="$LOCATION live=1" sync=false enable-last-buffer=false > /dev/null &
+}
+
+rtmp_sound()
+{
+	gst-launch v4l2src always-copy=false chain-ipipe=true ! \
+		capsfilter caps=video/x-raw-yuv,format='(fourcc)'NV12,width=$WIDTH,height=$HEIGHT,framerate='(fraction)'$FPS$PITCH ! \
+		dmaiaccel ! \
+		dmaienc_h264 copyOutput=false ddrbuf=false encodingpreset=2 ratecontrol=2 single-nalu=true targetbitrate=$BITRATE maxbitrate=4000000 level=51 intraframeinterval=150 idrinterval=250 t8x8intra=true t8x8inter=true bytestream=false headers=false ! \
+		queue ! \
+		flvmux streamable=true name=mux \
+		alsasrc latency-time=200000 buffer-time=4000000 slave-method=3 do-timestamp=false provide-clock=false ! \
+		capsfilter caps=audio/x-raw-int,channels=1,rate=16000,endianness=1234,signed=true,width=16,depth=16 ! \
+		queue ! \
+		dmaienc_aac bitrate=48000 maxbitrate=48000 tns=false copyOutput=false outputBufferSize=500000 fullbandwidth=true fixTimestamp=true ! \
+		mux. \
+		mux. ! \
+		queue ! \
+		rtmpsink location="$LOCATION live=1" sync=false enable-last-buffer=false > /dev/null &
+}
+
+case "$1" in
+start)
+	/usr/bin/killall -INT gst-launch-0.10 > /dev/null 2>&1
+	if [ "$SOUND" = "1" ] ; then
+		rtmp_sound
+	else
+		rtmp_no_sound
+	fi
 	echo "RTMP" > /tmp/onair
 	;;
 
