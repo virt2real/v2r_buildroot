b5dcfb7ac7da994b04ff9be81250ab8385293c1b
Gol <hypergol@gmail.com>
2015-01-03 16:07:17 +0400

    Fixed staging install for gst extension build

--- package/zbar/zbar.mk
+++ package/zbar/zbar.mk
@@ -7,6 +7,7 @@
 ZBAR_VERSION = HEAD
 ZBAR_SITE = http://github.com/ZBar/ZBar/tarball/$(V4L2GRAB_VERSION)
 ZBAR_INSTALL_TARGET = YES
+ZBAR_INSTALL_STAGING = YES
 ZBAR_AUTORECONF = YES
 ZBAR_DEPENDENCIES = libv4l
 ZBAR_CONF_OPT = --without-imagemagick --without-qt --without-gtk --without-python --without-x --without-jpeg --enable-shared=yes
