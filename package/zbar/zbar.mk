3cb4c13bece81e1a068ffbde178f4eabfcdac698
Gol <hypergol@gmail.com>
2015-01-03 15:45:39 +0400

    Fixed autoinstall

--- package/zbar/zbar.mk
+++ package/zbar/zbar.mk
@@ -12,16 +12,10 @@ ZBAR_DEPENDENCIES = libv4l
 ZBAR_CONF_OPT = --without-imagemagick --without-qt --without-gtk --without-python --without-x --without-jpeg --enable-shared=yes
 #--enable-pthread=no 
 
-define ZBAR_INSTALL_TARGET_CMDS
+ZBAR_POST_BUILD_HOOKS += ZBAR_INSTALL_FIXUP
 
-	mkdir -p $(TARGET_DIR)/usr/bin
-	mkdir -p $(TARGET_DIR)/usr/lib
-	$(INSTALL) -m 0755 $(@D)/zbar/.libs/libzbar.so.* $(TARGET_DIR)/usr/lib
-	$(INSTALL) -m 0755 $(@D)/zbarcam/zbarcam $(TARGET_DIR)/usr/bin
-endef
-
-define ZBAR_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/lib/libzbar.so.*
+define ZBAR_INSTALL_FIXUP
+	touch $(@D)/doc/man/zbarcam.1
 endef
 
 $(eval $(autotools-package))
