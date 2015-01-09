60727137b45941da3eeb05999aabfe8a3502f64b
Gol <hypergol@gmail.com>
2015-01-10 00:22:00 +0400

    New version, fixed capture-preview error

--- package/gphoto2/gphoto2.mk
+++ package/gphoto2/gphoto2.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-GPHOTO2_VERSION = 2.5.2
+GPHOTO2_VERSION = 2.5.6
 GPHOTO2_SOURCE = gphoto2-$(GPHOTO2_VERSION).tar.gz
-GPHOTO2_SITE = http://optimate.dl.sourceforge.net/project/gphoto/gphoto/2.5.2/
+GPHOTO2_SITE = http://optimate.dl.sourceforge.net/project/gphoto/gphoto/2.5.6/
 GPHOTO2_INSTALL_STAGING = YES
 GPHOTO2_INSTALL_TARGET = YES
 GPHOTO2_DEPENDENCIES = popt libusb-compat libgphoto2 libexif
