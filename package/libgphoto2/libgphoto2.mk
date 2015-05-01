302cacd3b67c2bff944968f44d0094526479fab8
Eugene Agafonov <e.a.agafonov@gmail.com>
2015-05-01 21:41:42 +0000

    Fix libgphoto2 build
    libgphoto2 uses unconditionally xml2 but it is not listed in build
    dependencies so fsbuild (from scratch) is failed

--- package/libgphoto2/libgphoto2.mk
+++ package/libgphoto2/libgphoto2.mk
@@ -10,7 +10,7 @@ LIBGPHOTO2_SITE = http://optimate.dl.sourceforge.net/project/gphoto/libgphoto/2.
 LIBGPHOTO2_INSTALL_STAGING = YES
 LIBGPHOTO2_INSTALL_TARGET = YES
 LIBGPHOTO2_CONF_OPT = --enable-static
-LIBGPHOTO2_DEPENDENCIES = libusb-compat libtool
+LIBGPHOTO2_DEPENDENCIES = libusb-compat libtool libxml2
 
 
 $(eval $(autotools-package))
