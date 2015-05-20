4e33bc9e765fd2e2683b64d48a594e693ede54bf
Gol <hypergol@gmail.com>
2015-05-20 15:13:56 +0300

    gphoto2 updated

--- package/libgphoto2/libgphoto2.mk
+++ package/libgphoto2/libgphoto2.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-LIBGPHOTO2_VERSION = 2.5.5
+LIBGPHOTO2_VERSION = 2.5.7
 LIBGPHOTO2_SOURCE = libgphoto2-$(LIBGPHOTO2_VERSION).tar.gz
-LIBGPHOTO2_SITE = http://optimate.dl.sourceforge.net/project/gphoto/libgphoto/2.5.5/
+LIBGPHOTO2_SITE = http://optimate.dl.sourceforge.net/project/gphoto/libgphoto/2.5.7/
 LIBGPHOTO2_INSTALL_STAGING = YES
 LIBGPHOTO2_INSTALL_TARGET = YES
 LIBGPHOTO2_CONF_OPT = --enable-static
