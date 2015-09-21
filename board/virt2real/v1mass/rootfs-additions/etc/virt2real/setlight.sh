7828b175d10c273afafdfca5fa8b5c195f96d904
Gol <hypergol@gmail.com>
2015-09-22 01:31:56 +0300

    Added leds for debug shield

--- board/virt2real/v1mass/rootfs-additions/etc/virt2real/setlight.sh
+++ board/virt2real/v1mass/rootfs-additions/etc/virt2real/setlight.sh
@@ -29,4 +29,28 @@ blue)
         echo "set blue $VALUE"
         echo $VALUE > /proc/v2r_gpio/pwctr3
         ;;
+debuggreen)
+        echo "set debug green $VALUE"
+        echo $VALUE > /proc/v2r_pins/77
+        ;;
+debugred)
+        echo "set debug red $VALUE"
+        echo $VALUE > /proc/v2r_pins/75
+        ;;
+debugblue)
+        echo "set debug blue $VALUE"
+        echo $VALUE > /proc/v2r_pins/78
+        ;;
+debugyellow)
+        echo "set debug yellow $VALUE"
+        echo $VALUE > /proc/v2r_pins/80
+        ;;
+rj45yellow)
+        echo "set debug yellow $VALUE"
+        echo $VALUE > /proc/v2r_pins/63
+        ;;
+rj45green)
+        echo "set debug yellow $VALUE"
+        echo $VALUE > /proc/v2r_pins/64
+        ;;
 esac
